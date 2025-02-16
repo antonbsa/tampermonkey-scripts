6f506b0e4bc2799d682533aed0a24b0754363f4c
Anton B <antonbsa.bck@gmail.com>
2025-02-16 16:49:48 -0300

    feat: add the news script

---
+++ scripts/thenews_highlightSponsoredContents.js
@@ -0,0 +1,18 @@
+const highlightMatchingElements = () => {
+  const contentBlocks = document.getElementById('content-blocks');
+  const directChildren = Array.from(contentBlocks.children);
+
+  directChildren.map(child => {
+    const firstBold = child.querySelector('b'); // first <b> inside this child (title)
+    if (firstBold && firstBold.textContent.includes('APRESENTADO POR')) {
+      child.style.backgroundColor = '#f56565'; // tailwindcss red-500
+    }
+  });
+}
+
+// Run the function if page is already loaded, or add an event listener otherwise
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', highlightMatchingElements);
+} else {
+  highlightMatchingElements();
+}
